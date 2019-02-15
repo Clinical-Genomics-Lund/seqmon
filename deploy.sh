ff98ccb4ae0d62560c92c8f310150d28bb38cfe1
Petter Storm <petter.storm@med.lu.se>
2019-02-15 16:59:27 +0100

    added plotLoad.R

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
+# Generate and copy plots to seqmon
 cp illuminaseqstat.R /data/bnf/proj/seqmon
+cp plotLoad.R /data/bnf/proj/seqmon
 Rscript /data/bnf/proj/seqmon/illuminaseqstat.R
+Rscript /data/bnf/proj/seqmon/plotLoad.R
 
 # Deploy web page
 scp www/seqmon.html pi@10.0.224.47:/home/pi/seqmon/
